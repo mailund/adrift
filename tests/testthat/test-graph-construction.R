5902b6836f0f8d9300898f3c85589eb26d1a2239
Thomas Mailund <mailund@birc.au.dk>
2018-05-05 09:44:33 +0200

    more testing setup

--- tests/testthat/test-graph-construction.R
+++ tests/testthat/test-graph-construction.R
@@ -8,4 +8,11 @@ test_that("we can construct a simple graph", {
 
   g$connect_nodes("a", "b")
   expect_true(g$connected)
+
+  expect_equal(g$get_children("a"), "b")
+  expect_equal(g$get_parents("b"), "a")
+  skip("we cannot yet access is_leaf vector by name")
+  #expect_true(g$is_leaf["b"])
+  #expect_false(g$is_leaf["a"])
+  expect_equal(g$is_leaf, c(FALSE, TRUE))
 })
